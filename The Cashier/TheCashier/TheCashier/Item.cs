77fbc2b49303c5a2437c7a8c2d203b0470fc0dde
mahardikasaputra <92503297+mahardikasaputra@users.noreply.github.com>
2021-10-14 13:16:52 +0700

    Add files via upload

---
+++ The Cashier/TheCashier/TheCashier/Item.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Cashier
+{
+    class Item
+    {
+        private int id;
+        public string title { get; set; }
+        public int quantity { get; set; }
+        public double price { get; set; }
+        public double subtotal { get; set; }
+        private string type;
+
+        public Item(int id, string title, int quantity, string type, double price)
+        {
+            this.id = id;
+            this.title = title;
+            this.quantity = quantity;
+            this.price = price;
+            this.subtotal = 0;
+            this.type = type;
+        }
+
+        public string getTitle()
+        {
+            return title;
+        }
+        public int getQuantity()
+        {
+            return quantity;
+        }
+        public string getType()
+        {
+            return type;
+        }
+        public double getPrice()
+        {
+            return price;
+        }
+        public double getSubtotal()
+        {
+            subtotal = price * quantity;
+            return subtotal;
+        }
+    }
+}
